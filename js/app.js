36beee78d8ff03c01952714ad6bdbb880936c571
slayerrr1985 <prubiera1985@gmail.com>
2018-09-01 22:33:25 +0200

    Basic functionalities + Specs

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ startGame();
 
 
 
-
 /*
  * Create a new deck of cards
  * cardTypes is an array with the different symbols/classes
@@ -118,7 +117,28 @@ function resetMatches(){
 }
 
 /*
+ *  Star Rating
+ * 
+    The game displays a star rating (from 1 to at least 3) that reflects the player's performance. At the beginning of a game, it should display at least 3 stars. After some number of moves, it should change to a lower star rating. After a few more moves, it should change to a even lower star rating (down to 1).
+
+    The number of moves needed to change the rating is up to you, but it should happen at some point.
+ * 
+ */
+
+function updateStars(){
+
+}
+
+function resetStars(){
+
+}
+
+
+/*
  * Manage what happens when we win the game
+ * 
+ * When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. It should also tell the user how much time it took to win the game, and what the star rating was.
+ * 
  */
 
 function checkEndGame(){
@@ -127,7 +147,6 @@ function checkEndGame(){
         stopTimeCounter(timerId);
     }
 }
-
 
 /*
  * Start a new game
@@ -147,8 +166,6 @@ function startGame(){
     /*shuffle(deck);*/
     displayDeck(deck);
 }
-
-
 
 /*
  * Function to run if a card is clicked:
@@ -192,7 +209,3 @@ function cardClick(e){
         }
     }
 }
-
-
-
-
